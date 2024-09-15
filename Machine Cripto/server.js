1c8da41444d7695560a3f4a41308750a576660d9
ALISSON ALVES <alissonvg42777@gmail.com>
2024-09-15 16:52:13 -0300

    Add files via upload

---
+++ Machine Cripto/server.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function () {
+  document.getElementById('cadCliente').addEventListener('tabelaCliente').getElementsById('tbody')[0];
+  const novoCliente = table.insertRow()
+
+  novoCliente.innerHTML = `
+  <td><input class="X">
+  <td><input class="Y">
+  <td><input class="Z">
+  `
+});
+
